12d91a2f2ca8ce48971405d6bd94b562f7f6ac15
Gaz <garethw738@gmail.com>
2023-08-12 14:58:00 +0200

    Addec lightgbm

--- make_prediction_lightgbm.py
+++ make_prediction_lightgbm.py
@@ -3,7 +3,7 @@ from sklearn.preprocessing import LabelEncoder
 import lightgbm as lgb
 
 # Load the training data
-train_data = pd.read_csv('Train1.csv')
+train_data = pd.read_csv('Train.csv')
 
 # Handling Missing Values for Numerical Columns
 train_data.fillna(train_data.mean(numeric_only=True), inplace=True)
@@ -11,17 +11,17 @@ train_data.fillna(train_data.mean(numeric_only=True), inplace=True)
 # Encoding Categorical Variables
 label_encoders = {}
 for column in train_data.select_dtypes(include=['object']).columns:
-    if column not in ['person_id', 'survey_date']:
+    if column not in ['Person_id', 'Survey_date']:
         le = LabelEncoder()
         train_data[column] = le.fit_transform(train_data[column])
         label_encoders[column] = le
 
 # Removing unnecessary columns
-train_data.drop(columns=['person_id', 'survey_date'], inplace=True)
+train_data.drop(columns=['Person_id', 'Survey_date'], inplace=True)
 
 # Separating the dependent and independent variables
-X_train = train_data.drop(columns=['target'])
-y_train = train_data['target']
+X_train = train_data.drop(columns=['Target'])
+y_train = train_data['Target']
 
 # Creating the LightGBM model
 lightgbm_model = lgb.LGBMClassifier(random_state=42)
@@ -30,7 +30,7 @@ lightgbm_model = lgb.LGBMClassifier(random_state=42)
 lightgbm_model.fit(X_train, y_train)
 
 # Load the test data
-test_data = pd.read_csv('Test1.csv')
+test_data = pd.read_csv('Test.csv')
 
 # Handling Missing Values for Numerical Columns in the test data
 test_data.fillna(train_data.mean(numeric_only=True), inplace=True)
@@ -42,14 +42,14 @@ for column, le in label_encoders.items():
     test_data[column] = test_data[column].apply(lambda x: le.transform([x])[0] if x in le.classes_ else -1)
 
 # Removing unnecessary columns from the test data
-test_data_processed = test_data.drop(columns=['person_id', 'survey_date'])
+test_data_processed = test_data.drop(columns=['Person_id', 'Survey_date'])
 
 # Making predictions on the test data (predicting probabilities for the positive class)
 y_test_prob_pred_lgb = lightgbm_model.predict_proba(test_data_processed)[:, 1]
 
 # Creating a DataFrame with "person_id" and the predicted probability of unemployment
 predictions_prob_df = pd.DataFrame({
-    'person_id': test_data['person_id'],
+    'Person_id': test_data['Person_id'],
     'Probability_Unemployed': y_test_prob_pred_lgb
 })
 
